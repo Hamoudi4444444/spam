97dfe15c18665dec8e2d4543095f7939d1bf63aa
Hamoudi4444444 <44705191+Hamoudi4444444@users.noreply.github.com>
2018-11-09 16:55:39 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "510143396862492672"; // 
+var channel = "510436260452630538";//
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
